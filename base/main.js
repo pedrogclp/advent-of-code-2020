71e6d4876c5bfdde149351d4a4f6425da3f537f1
Pedro Jesús García Cabello <peter@MBP-de-Pedro.technicolor.net>
2020-12-02 23:11:24 +0100

    Base code

---
+++ base/main.js
@@ -0,0 +1,15 @@
+const inputText = `
+`;
+
+function parseInput(inputText) {
+  return inputText;
+}
+
+function resolve1(input) {
+  return input;
+}
+
+const input = parseInput(inputText);
+const output = resolve1(input);
+
+console.log(output);
